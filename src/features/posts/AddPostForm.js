3044afece735ec914e9f20661a62b7cc688f0234
Matheos Profirescu <matheosprofirescu@yahoo.com>
2023-04-17 19:31:44 +0300

    api call

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,28 +1,43 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { postAdded } from './postsSlice'
-import { selectAllUsers } from '../users/usersSlice'
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { addNewPost } from "./postsSlice";
+import { selectAllUsers } from "../users/usersSlice";
 
 const AddPostForm = () => {
     const dispatch = useDispatch()
 
-    const users = useSelector(selectAllUsers)
-
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
     const [userId, setUserId] = useState('')
+    const [addRequestStatus, setAddRequestStatus] = useState('idle')
+
+    const users = useSelector(selectAllUsers)
 
     const onTitleChanged = e => setTitle(e.target.value)
     const onContentChanged = e => setContent(e.target.value)
     const onAuthorChanged = e => setUserId(e.target.value)
 
-    const onSavePostClicked = () => {
-        if (title && content) {
-            dispatch(postAdded(title, content, userId))
-        }
-    }
 
-    const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+    const canSave = [title, content, userId].every(Boolean) && addRequestStatus === 'idle';
+
+    const onSavePostClicked = () => {
+        if (canSave) {
+            try {
+                setAddRequestStatus('pending')
+                dispatch(addNewPost({ title, body: content, userId })).unwrap()
+
+                setTitle('')
+                setContent('')
+                setUserId('')
+            } catch (err) {
+                console.error('Failed to save the post', err)
+            } finally {
+                setAddRequestStatus('idle')
+            }
+        }
+
+    }
 
     const usersOptions = users.map(user => (
         <option key={user.id} value={user.id}>
@@ -31,24 +46,26 @@ const AddPostForm = () => {
     ))
 
     return (
-        <div>
-            <h1>Add Post</h1>
+        <section>
+            <h2>Add a New Post</h2>
             <form>
+                <label htmlFor="postTitle">Post Title:</label>
+                <input
+                    type="text"
+                    id="postTitle"
+                    name="postTitle"
+                    value={title}
+                    onChange={onTitleChanged}
+                />
                 <label htmlFor="postAuthor">Author:</label>
                 <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
                     <option value=""></option>
                     {usersOptions}
                 </select>
-                <label htmlFor="title">Title</label>
-                <input
-                    type="text"
-                    id="title"
-                    value={title}
-                    onChange={onTitleChanged}
-                />
-                <label htmlFor="content">Content</label>
+                <label htmlFor="postContent">Content:</label>
                 <textarea
-                    id="content"
+                    id="postContent"
+                    name="postContent"
                     value={content}
                     onChange={onContentChanged}
                 />
@@ -56,12 +73,9 @@ const AddPostForm = () => {
                     type="button"
                     onClick={onSavePostClicked}
                     disabled={!canSave}
-                >
-                    Save Post
-                </button>
+                >Save Post</button>
             </form>
-        </div>
+        </section>
     )
 }
-
 export default AddPostForm
