5c5d0fcbb7d03b2e874894205abaec92ff7b7fb6
Matheos Profirescu <matheosprofirescu@yahoo.com>
2023-04-19 12:34:06 +0300

    optimization of performance

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { getCount, incrementCount } from '../features/posts/postsSlice'
 
 const Header = () => {
+    const count = useSelector(getCount)
+    const dispatch = useDispatch()
+
   return (
     <header className='Header'>
         <h1>Redux</h1>
@@ -9,7 +14,9 @@ const Header = () => {
             <ul>
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='post'>Post</Link></li>
+                <li><Link to='user'>Users</Link></li>
             </ul>
+            <button onClick={() => dispatch(incrementCount())}>{count}</button>
         </nav>
     </header>
     )
